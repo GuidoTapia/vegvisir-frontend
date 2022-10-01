326371ba6291065d075acd67c7b2a9a0e3bc72f6
Guido Tapia <guido.tapia@ucsp.edu.pe>
2022-10-01 13:30:53 -0500

    feat: welcome screen

---
+++ src/screens/Welcome.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+export default function Welcome() {
+  return (
+    <div className="w-screen h-screen">
+      <Layout />
+    </div>
+  );
+}
