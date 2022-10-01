f74191f005d5c4b768b7b6370cd865b71e1bffc1
Guido Tapia <guido.tapia@ucsp.edu.pe>
2022-10-01 13:33:45 -0500

    fix: router file name fix and routes

--- src/shared/AppRouter.jsx
+++ src/shared/AppRouter.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "../Login";
+import Areas from "../screens/Areas";
+import Times from "../screens/Times";
+import Welcome from "../screens/Welcome";
 import paths from "./paths";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path={paths.login} element={<Login />} />
+      <Route path={paths.welcome} element={<Welcome />} />
+      <Route path={paths.times} element={<Times />} />
+      <Route path={paths.areas} element={<Areas />} />
     </Routes>
   );
 };
--- src/shared/paths.js
+++ src/shared/paths.js
@@ -3,5 +3,7 @@ const paths = {
   error: "/error",
   login: "/login",
   welcome: "/welcome",
+  times: "/times",
+  areas: "/areas",
 };
 export default paths;
