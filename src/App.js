c095167eee1df6dab7df0b857597434bbc3b5bbd
Guido Tapia <guido.tapia@ucsp.edu.pe>
2022-10-01 13:34:04 -0500

    fix: primereact styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./shared/AppRouter";
+import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
+import "primereact/resources/primereact.min.css"; //core css
+import "primeicons/primeicons.css";
 
 export default function App() {
   return (
